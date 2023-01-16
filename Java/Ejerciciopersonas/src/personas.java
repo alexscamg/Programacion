8855b32483341ebc5276af474da2378a42bdc27b
alexscamg <alejandrosanchezsk8@gmail.com>
2023-01-16 20:06:25 +0100

    Commit inicial

---
+++ Java/Ejerciciopersonas/src/personas.java
@@ -0,0 +1,17 @@
+
+public class personas {
+	
+public String nombre;
+public personas(String nombre, String apellidos, int edad, boolean casado, String numeroDocumentoIdentidad) {
+	super();
+	this.nombre = nombre;
+	this.apellidos = apellidos;
+	this.edad = edad;
+	this.casado = casado;
+	this.numeroDocumentoIdentidad = numeroDocumentoIdentidad;
+}
+public String apellidos;
+public int edad;
+private boolean casado;
+public String numeroDocumentoIdentidad;
+}
---
+++ Java/ejerciciomedico/src/ejerciciomedico/Medico.java
@@ -0,0 +1,23 @@
+package ejerciciomedico;
+
+public class Medico {
+	private String nombre;
+	private String apellidos;
+	private int edad;
+	private boolean casado;
+	private String numeroDocumentoIdentidad;
+	private String especialidad;
+	
+	
+	//CONSTRUCTOR
+		public Medico() {
+			nombre= "Perico";
+			apellidos = "Javinco";
+			edad = 20;
+			casado = true;
+			numeroDocumentoIdentidad = "9362839F";
+			
+			
+			
+		}
+}
