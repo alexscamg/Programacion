6094f9825989fdcb5182ed9344172a48f32e4cf1
alexscamg <alejandrosanchezsk8@gmail.com>
2023-01-26 21:03:43 +0100

    Commit

--- Java/Fundamentos/HolaMundo.java
+++ Java/Fundamentos/HolaMundo.java
@@ -2,6 +2,6 @@ package clase1;
 
 public class HolaMundo{
 	public static void main (String [] args){
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo")
 }
 }
---
+++ Java/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,9 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+	}
+ //TODO Hola Lidia, en 2 horas con el kanka ya he aprendido mas que contigo en todo el curso
+}
