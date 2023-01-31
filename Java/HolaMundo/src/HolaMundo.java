1ef46d004cfd9fd0150991da58b9e71ff9bb8d57
alexscamg <alejandrosanchezsk8@gmail.com>
2023-01-31 17:30:39 +0100

    Commit!

--- Java/HolaMundo/src/HolaMundo.java
+++ Java/HolaMundo/src/HolaMundo.java
@@ -5,5 +5,5 @@ public class HolaMundo {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 	}
- //TODO Hola Lidia, en 2 horas con el kanka ya he aprendido mas que contigo en todo el curso
+
 }
