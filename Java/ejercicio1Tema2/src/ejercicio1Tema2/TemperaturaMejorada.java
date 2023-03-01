0a0b55b1b847cf69d83a75134d258706b254d798
Alex'S <75066878+alexscamg@users.noreply.github.com>
2023-03-01 12:46:51 +0100

    Add files via upload

---
+++ Java/ejercicio1Tema2/src/ejercicio1Tema2/TemperaturaMejorada.java
@@ -0,0 +1,17 @@
+package ejercicio1Tema2;
+
+
+public class TemperaturaMejorada {
+	double tempActual;
+	
+	void darTemperaturaActual(double temperaturaActual) {
+		this.tempActual = temperaturaActual;
+	}
+	
+	double devolverTemperatura() {
+		return this.tempActual; 
+		
+		
+	}
+
+}
