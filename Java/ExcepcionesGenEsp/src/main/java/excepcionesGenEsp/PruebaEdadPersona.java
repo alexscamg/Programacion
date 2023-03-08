708f24e56bd0657b6c0063e2ab7e5c1549ca60a9
alexscamg <alejandrosanchezsk8@gmail.com>
2023-03-08 12:10:40 +0100

    Terminado Ejercicio Excepciones

--- Java/ExcepcionesGenEsp/src/main/java/excepcionesGenEsp/PruebaEdadPersona.java
+++ Java/ExcepcionesGenEsp/src/main/java/excepcionesGenEsp/PruebaEdadPersona.java
@@ -5,9 +5,9 @@ public class PruebaEdadPersona {
 	
 	public void generaExcepcionEdad(int edad) throws InfantilException, AdultoException, MayorException{
 		if (edad < 18) {
-			throw new InfantilException("Es infantil");
+			throw new InfantilException("Es infantil, menor de 18 años");
 		}else if (edad >= 18 && edad < 65) {
-			throw new AdultoException("Es mayor de edad");
+			throw new AdultoException("Es mayor de edad, mayor de 18 y menor de 65");
 		}else if(edad>=65) {
 			throw new MayorException("Es mayor de 65");
 		}
